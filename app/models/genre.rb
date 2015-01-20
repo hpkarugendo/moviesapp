6f8f1704beadf42e181d26dbdc1eeaa86cfc8195
Henry Patrick Karguendo <c12716891@mydit.ie>
2015-01-20 14:34:10 +0000

    Initial commit

---
+++ app/models/genre.rb
@@ -0,0 +1,4 @@
+class Genre < ActiveRecord::Base
+	has_many :movies, :dependent => :destroy
+	validates :name, presence: true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviesapp::Application.config.secret_token = '8f7da6d1bb8189b06acb520f957a697f2af519b026ef9afff2142ef0b1a83e3da957f831137320f18f2f9c2b1045a0c606bb9f4d3c5755bbb5e08da0f8be1a0e'
